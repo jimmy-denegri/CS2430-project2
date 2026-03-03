f318f9d4886da1dc49c6465c9f4a4376b9e18e7a
k4nch1t4 <jimmy_denegri@hotmail.com>
2026-03-02 20:43:36 -0700

    UniversalSet was implemented and it's ready to instantiate in the main class.

--- classes/UniversalSet.java
+++ classes/UniversalSet.java
@@ -1,52 +1,71 @@
 package classes;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class UniversalSet {
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 	
-	// Stores elements in fixed order
-    private List<String> elements;
+
     
 
-    // Constructor
+    // Fixed ordered list of elements
+    private List<String> elements;
+
+    // Fast lookup map: element name -> index
+    private Map<String, Integer> indexMap;
+
+    /*
+     * Constructor
+     * - Requires at least 10 elements
+     * - No duplicates allowed
+     */
     public UniversalSet(List<String> elements) {
-        // TODO: implement
+
+        if (elements.size() < 10) {
+             System.out.println("Universal set must contain at least 10 elements.");
+        }
+
+        this.elements = new ArrayList<>();
+        this.indexMap = new HashMap<>();
+
+        for (int i = 0; i < elements.size(); i++) {
+
+            String name = elements.get(i);
+
+            if (indexMap.containsKey(name)) {
+                System.out.println("Duplicate element: " + name);
+            }
+
+            this.elements.add(name);
+            this.indexMap.put(name, i);
+        }
     }
 
     // Returns number of elements
     public int size() {
-        // TODO: implement
-        return 0;
+        return elements.size();
     }
 
-    // Returns list of elements
+    // Returns list of elements 
     public List<String> getElements() {
-        // TODO: implement
-        return null;
+        return new ArrayList<>(elements);
     }
 
-    // Returns index of element
-    public int indexOf(String elementName) {
-        // TODO: implement
-        return 0;
-    }
-
-    // Checks if element exists
+    // Returns true if element exists in universe
     public boolean contains(String elementName) {
-        // TODO: implement
-        return false;
+        return indexMap.containsKey(elementName);
+    }
+
+    // Returns index of element in universe
+    public int indexOf(String elementName) {
+
+        Integer index = indexMap.get(elementName);
+
+        return index;
     }
 
     @Override
     public String toString() {
-        // TODO: implement
-        return "";
+        return "U=" + elements;
     }
-
 }
